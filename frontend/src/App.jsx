0a54ff5f83d7968f0ddc06b9d263318ef7d0b4ae
heyadin <adinahmad003@gmail.com>
2025-05-28 17:38:57 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Cart } from './pages/Cart'
 import { Dashboard } from './pages/Dashboard'
 import { Services } from './pages/Services'
+import { Suspense } from 'react'
 
 function App() {
 
   return (
     <div className='bg-gray-500 '>
-      {/* <Dashboard /> */}
-      {/* <Services/> */}
-      <Cart/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Suspense fallback={"...Loading"}><Dashboard /></Suspense>} />
+        <Route path='/cart' element={<Suspense fallback={"...Loading"}><Cart /></Suspense>} />
+        <Route path='/services' element={<Suspense fallback={"...Loading"}><Services /></Suspense>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom'
 import wokabeltLogo from '../assets/Wokabelt_logo.png'
 export function AppBar() {
+    const navigate = useNavigate()
     return <div className="flex justify-between items-center px-24 py-2 w-full bg-[#F4F4F4] font-family shadow-md">
-        <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-4'onClick={()=>{navigate('/')}}>
             <img className='rounded-full overflow-hidden h-12.5 w-12.5' src={wokabeltLogo} alt="" />
             <div className='cursor-pointer'>
                 <div className='text-[28px] font-[900] text-[#333] '><b>Wokabelt</b></div>
@@ -9,8 +11,8 @@ export function AppBar() {
             </div>
         </div>
         <div className="flex gap-15 text-lg items-center ">
-            <div className='cursor-pointer hover:text-[#0063CD]'>Services</div>
-            <div className='cursor-pointer hover:text-[#0063CD]'>Cart</div>
+            <div className='cursor-pointer hover:text-[#0063CD]'onClick={()=>{navigate('/services')}}>Services</div>
+            <div className='cursor-pointer hover:text-[#0063CD]'onClick={()=>{navigate('/cart')}}>Cart</div>
             <div className='cursor-pointer hover:text-[#0063CD]'>Learning</div>
             <div className='cursor-pointer hover:text-[#0063CD]'>Login</div>
             <div className='cursor-pointer px-[13px] pb-1 rounded-sm text-white bg-[#1e3c72] hover:bg-[#666] '>Signup</div>
